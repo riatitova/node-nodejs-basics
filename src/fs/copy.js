e0ff9b61a0af2eb13fd85d75af41a7c90cd8631e
Mariya Titova <mariya.titova@simbirsoft.com>
2022-06-05 21:46:42 +0300

    feat: add create function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,22 @@
+// import * as fs from 'fs';
+// import * as asyncFs from 'fs/promises';
+
+// const folderExists = path => asyncFs.access(path, fs.constants.R_OK | fs.constants.W_OK).then(() => true, () => false);
+
 export const copy = async () => {
-    // Write your code here 
+    // Write your code here
+    // const source = './files';
+    // const destination = './files_copy';
+
+    // let targetFile = destination;
+
+    // If target is a directory, a new file with the same name will be created
+    // if (await folderExists(destination)) {
+    //     if (fs.lstatSync(destination).isDirectory()) {
+    //         targetFile = path.join(target, path.basename(source));
+    //     }
+    // }
+
 };
+
+// copy().then();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,15 @@
+import * as fs from 'fs/promises';
+
+const fileExists = path => fs.stat(path).then(() => true, () => false);
+
 export const create = async () => {
-    // Write your code here 
+    // Write your code here
+    if (await fileExists('./files/fresh.txt')) {
+        throw new Error('FS operation failed');
+    } else {
+        const content = 'I am fresh and young';
+        await fs.writeFile('./files/fresh.txt', content);
+    }
 };
+
+create().then();
